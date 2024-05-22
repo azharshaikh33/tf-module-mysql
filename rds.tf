8a6311eff180fb109a93e9f62683e5d6606496a1
Azhar <azharshaikh33@gmail.com>
2024-05-22 11:08:37 +0400

    Auto Push

--- rds.tf
+++ rds.tf
@@ -1,13 +1,15 @@
 resource "aws_db_instance" "mysql" {
-  allocated_storage    = 10
-  db_name              = "mydb"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  username             = "admin1"
-  password             = "RoboShop1"
-  parameter_group_name = aws_db_parameter_group.mysql_pg.name
-  skip_final_snapshot  = true
+  allocated_storage      = 10
+  db_name                = "mydb"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t3.micro"
+  username               = "admin1"
+  password               = "RoboShop1"
+  parameter_group_name   = aws_db_parameter_group.mysql_pg.name
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.mysql_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.allow_mysql.id]
 }
 
 resource "aws_db_parameter_group" "mysql_pg" {
