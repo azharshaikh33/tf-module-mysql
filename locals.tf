33e0caf89f64b5bbb90098c3b9467488a851c890
Azhar <azharshaikh33@gmail.com>
2024-06-05 12:38:02 +0400

    Auto Push

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 # creating locals
 
 locals {
-  MYSQL_USER = jsondecode(aws_secretsmanager_secret_version.secrets.secret_string)["MYSQL_USERNAME"]
-  MYSQL_PASS = jsondecode(aws_secretsmanager_secret_version.secrets.secret_string)["MYSQL_PASSWORD"]
+  MYSQL_USER = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["RDS_USERNAME"]
+  MYSQL_PASS = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["RDS_PASSWORD"]
 }
